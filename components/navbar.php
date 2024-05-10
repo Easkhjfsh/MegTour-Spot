0221144f4df135c7ecf43e601982b6774659954d
Easkhjfsh <edilakhon@gmail.com>
2024-05-10 10:58:11 -0700

    first commit

---
+++ components/navbar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="nav">
+            <div class="logo"><a href="#">MegTour-Spot</a></div>
+            <ul>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+
+</html>
